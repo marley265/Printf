d0d457da9383809bad8a1c49d1460965d2fedbf2
Jack Ndiritu <125997205+marley265@users.noreply.github.com>
2023-04-28 15:24:01 +0300

    Update main.h

--- main.h
+++ main.h
@@ -5,6 +5,15 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 /**
  * struct flags - struct containing flags to "turn on"
  * when a flag specifier is passed to _printf()
